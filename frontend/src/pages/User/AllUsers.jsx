54431d9f1f0c198efcd8acc18b18e4213f955de0
KavishkaSasindu <sasindu0215@gmail.com>
2025-03-01 23:23:49 +0530

    updated authorization token logic

--- frontend/src/pages/User/AllUsers.jsx
+++ frontend/src/pages/User/AllUsers.jsx
@@ -1,11 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 const AllUsers = () => {
+  const navigate = useNavigate();
+
   const [users, setUsers] = useState([]);
 
   const fetchUsers = async () => {
     try {
+      const token = localStorage.getItem("jwt_token");
+      if (!token) {
+        navigate("/api/v1/auth/login");
+      }
       const response = await axios.get(
         "http://localhost:8080/api/v1/user/allUsers",
         {
@@ -17,7 +24,10 @@ const AllUsers = () => {
 
       const data = await response.data;
       console.log(data);
+      setUsers(data);
     } catch (error) {
+      localStorage.removeItem("jwt_token");
+      navigate("/api/v1/auth/login");
       console.log(error.message);
     }
   };
@@ -29,6 +39,28 @@ const AllUsers = () => {
   return (
     <div>
       <div>All Users</div>
+      <div className="w-[100%] flex justify-center items-center ">
+        <div className="w-[80%] flex justify-center items-center  ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {users.map((user, index) => (
+              <div
+                className="p-3  shadow-sm shadow-gray-200 space-y-5"
+                key={index}
+              >
+                <div>{user.username}</div>
+                <div className="flex space-x-2">
+                  <button className="px-3 py-1 bg-blue-200  rounded-sm">
+                    Update
+                  </button>
+                  <button className="px-3 py-1 bg-red-200  rounded-sm">
+                    Delete
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
