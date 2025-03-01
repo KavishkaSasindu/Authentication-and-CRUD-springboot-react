4ae2d42bb36577ef1c42edaf66f70e8c71a244d4
KavishkaSasindu <sasindu0215@gmail.com>
2025-03-01 23:35:08 +0530

    final update

--- Backend/src/main/java/com/example/Backend/jwt/JwtService.java
+++ Backend/src/main/java/com/example/Backend/jwt/JwtService.java
@@ -27,7 +27,7 @@ public class JwtService {
                 .subject(username)
                 .signWith(getKey())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis()+1000*60*15))
+                .expiration(new Date(System.currentTimeMillis()+1000*60))
                 .compact();
     }
 
